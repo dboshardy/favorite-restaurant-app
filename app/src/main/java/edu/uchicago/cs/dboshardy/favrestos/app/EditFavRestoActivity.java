c57f80929601d806036147ac7f29b698dee49fc1
dboshardy <dboshardy@uchicago.ed>
2014-05-31 17:02:43 -0500

    Added ids to fragment class.

--- app/src/main/java/edu/uchicago/cs/dboshardy/favrestos/app/EditFavRestoActivity.java
+++ app/src/main/java/edu/uchicago/cs/dboshardy/favrestos/app/EditFavRestoActivity.java
@@ -10,10 +10,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import edu.uchicago.cs.dboshardy.favRestos.app.R;
 
 public class EditFavRestoActivity extends ActionBarActivity {
+
+
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
@@ -38,13 +45,28 @@ public class EditFavRestoActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class EditFavRestoFragment extends Fragment {
+
+        private Button mCancelButton;
+        private Button mUpdateButton;
+        private Button mFetchButton;
+        private EditText mSearchResto;
+        private EditText mSearchCity;
+        private EditText mEditPhone;
+        private EditText mEditAddress;
+        private EditText mEditYelpURL;
+        private TextView mTextPhone;
+        private TextView mTextAddress;
+        private TextView mTextYelpURL;
+        private CheckBox mFavoriteCheckBox;
+        private View mFavoriteColorView;
+        private ImageView mImage;
 
         //TODO: Add viewgroups and instantiate them
         public EditFavRestoFragment() {
@@ -54,6 +76,18 @@ public class EditFavRestoActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_edit_fav_resto, container, false);
+            mCancelButton = (Button) rootView.findViewById(R.id.cancel_button);
+            mUpdateButton = (Button) rootView.findViewById(R.id.update_button);
+            mFetchButton = (Button) rootView.findViewById(R.id.fetch_button);
+            mImage = (ImageView) rootView.findViewById(R.id.image_view);
+            mEditPhone = (EditText) rootView.findViewById(R.id.edit_phone);
+            mEditAddress = (EditText) rootView.findViewById(R.id.edit_address);
+            mEditYelpURL = (EditText) rootView.findViewById(R.id.edit_yelp_url);
+            mTextPhone = (TextView) rootView.findViewById(R.id.text_phone);
+            mTextAddress = (TextView) rootView.findViewById(R.id.text_address);
+            mTextYelpURL = (TextView) rootView.findViewById(R.id.text_yelp_url);
+            mFavoriteCheckBox = (CheckBox) rootView.findViewById(R.id.favorite_checked);
+            mFavoriteColorView = rootView.findViewById(R.id.favorite_color_view);
             return rootView;
         }
     }
